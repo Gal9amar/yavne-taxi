7bea8e016203913d6f2a405fa8df96e32c221809
Gal Amar <106630289+Gal9amar@users.noreply.github.com>
2023-01-14 11:04:22 +0200

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+    function randomCheckbox() {
+        // Get all checkboxes
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        // Get a random checkbox
+        var randomCheckbox = checkboxes[Math.floor(Math.random() * checkboxes.length)];
+        // Toggle the checked property
+        randomCheckbox.checked = !randomCheckbox.checked;
+    }
+setInterval(randomCheckbox, 2000);
+
